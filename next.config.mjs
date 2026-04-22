2e7e382047805d90a233b0b0b79afd601eaddd95
pavinee23 <pavinee23@github.com>
2026-04-22 16:47:23 +0900

    fix: allow assets.zyrosite.com and flagcdn.com in next/image

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.zyrosite.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
