95fd726c94adb2f0614b392f71ec9c3cfbd563a5
pavinee23 <pavinee23@github.com>
2026-04-22 18:01:28 +0900

    fix: add missing currency, grafana, and MapComponent files for Vercel build

--- src/components/energy/MapComponent.tsx
+++ src/components/energy/MapComponent.tsx
@@ -2,6 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
 
 // Device type
